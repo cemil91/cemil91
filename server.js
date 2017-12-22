393f0d901797443fae94b4999cb5be8b3038000b
cemil91 <cemil_mm@mail.ru>
2017-12-22 19:46:25 +0400

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,18 @@ function heartbeat() {
 
 
 
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: "195.16.88.9",
+  user: "ilqar845_oyun",
+  password: "12345"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 
 var myVar;
