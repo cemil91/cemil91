3f59567209c544ef1962c003a7ff5be886159c44
cemil91 <cemil_mm@mail.ru>
2017-12-23 00:16:38 +0400

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ con.query("SELECT * FROM users", function (err, result, fields) {
 if (err) throw err;
 console.log(result);
 });
-    */                      }
+                         }*/ 
 
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
