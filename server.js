39eda2bd2d2893d938f059b87ab6b51c98b6dec9
cemil91 <cemil_mm@mail.ru>
2017-12-24 23:47:03 +0400

    Update server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ console.log('Получено сообщение ' + message);
 	
 	 if(res[0] == "oyna"){	
 
-if(clients[res[1]] != undefined){if(clients[res[1]].readyState === clients[res[1]].OPEN){if(clients[res[2]].readyState === clients[res[2]].OPEN)clients[res[1]].send("oyna:"+res[2]);}
+if(clients[res[1]] != undefined){if(clients[res[1]].readyState === clients[res[1]].OPEN){if(clients[res[2]].readyState === clients[res[2]].OPEN)clients[res[1]].send("oyna:"+res[2]);}}
 
 console.log('Получено сообщение ' + message);
                                }	
