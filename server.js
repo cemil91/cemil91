4e4132d7c52cd24985ec674338f9a1633c870602
cemil91 <cemil_mm@mail.ru>
2017-12-24 23:52:46 +0400

    Update server.js

--- server.js
+++ server.js
@@ -87,21 +87,21 @@ console.log('Получено сообщение ' + message);
 	
 	 if(res[0] == "teklif"){	
 
-if(clients[res[1]].readyState === clients[res[1]].OPEN){if(clients[res[2]].readyState === clients[res[2]].OPEN)clients[res[1]].send("teklif:"+res[2]);}
+if(clients[res[1]].readyState === clients[res[1]].OPEN){if(clients[res[2]] != undefined)clients[res[1]].send("teklif:"+res[2]);}
 
 console.log('Получено сообщение ' + message);
                                }
 	
 	 if(res[0] == "qebulolundu"){	
 
-if(clients[res[1]].readyState === clients[res[1]].OPEN){if(clients[res[2]].readyState === clients[res[2]].OPEN)clients[res[1]].send("qebulolundu:"+res[2]);}
+if(clients[res[1]].readyState === clients[res[1]].OPEN){if(clients[res[2]] != undefined)clients[res[1]].send("qebulolundu:"+res[2]);}
 
 console.log('Получено сообщение ' + message);
                                }
 	
 	 if(res[0] == "oyna"){	
 
-if(clients[res[1]] != undefined){if(clients[res[1]].readyState === clients[res[1]].OPEN){if(clients[res[2]].readyState === clients[res[2]].OPEN)clients[res[1]].send("oyna:"+res[2]);}}
+if(clients[res[1]] != undefined){if(clients[res[1]].readyState === clients[res[1]].OPEN){clients[res[1]].send("oyna:"+res[2]);}}
 
 console.log('Получено сообщение ' + message);
                                }	
